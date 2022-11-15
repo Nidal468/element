a019dcda9760c9733e06aade8b0299c229c9349f
Nidal 468 <115021956+Nidal468@users.noreply.github.com>
2022-11-15 11:13:13 +0000

    footer

--- mail.php
+++
@@ -1,8 +0,0 @@
-<?php
-$to = "nidal411511@gmail.com";
-$subject = "My subject";
-$headers = "From: webmaster@example.com" . "\r\n" .
-"CC: somebodyelse@example.com";
-
-mail($to,$subject,$txt,$headers);
-?>
